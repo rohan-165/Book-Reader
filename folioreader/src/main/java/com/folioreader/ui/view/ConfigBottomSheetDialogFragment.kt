2678a64558ef6a45a1eac39baa9040174e5c412c
JagaranAG2022 <jagaran@ambition.guru>
2024-11-13 15:17:06 +0545

    update ui of theme

--- folioreader/src/main/java/com/folioreader/ui/view/ConfigBottomSheetDialogFragment.kt
+++ folioreader/src/main/java/com/folioreader/ui/view/ConfigBottomSheetDialogFragment.kt
@@ -12,6 +12,7 @@ import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.FrameLayout
 import android.widget.SeekBar
+import android.widget.Switch
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.folioreader.Config
@@ -44,6 +45,7 @@ class ConfigBottomSheetDialogFragment : BottomSheetDialogFragment() {
     private lateinit var config: Config
     private var isNightMode = false
     private lateinit var activityCallback: FolioActivityCallback
+    private lateinit var themeSwitch: Switch
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -69,6 +71,7 @@ class ConfigBottomSheetDialogFragment : BottomSheetDialogFragment() {
         }
 
         config = AppUtil.getSavedConfig(activity)!!
+        isNightMode = config.isNightMode  // Set initial value based on config
         initViews()
     }
 
@@ -90,66 +93,88 @@ class ConfigBottomSheetDialogFragment : BottomSheetDialogFragment() {
             container.setBackgroundColor(ContextCompat.getColor(context!!, R.color.white))
         }
 
-        if (isNightMode) {
-            view_config_ib_day_mode.isSelected = false
-            view_config_ib_night_mode.isSelected = true
-            UiUtil.setColorIntToDrawable(
-                config.currentThemeColor,
-                view_config_ib_night_mode.drawable
-            )
-            UiUtil.setColorResToDrawable(R.color.app_gray, view_config_ib_day_mode.drawable)
-        } else {
-            view_config_ib_day_mode.isSelected = true
-            view_config_ib_night_mode.isSelected = false
-            UiUtil.setColorIntToDrawable(
-                config.currentThemeColor,
-                view_config_ib_day_mode!!.drawable
-            )
-            UiUtil.setColorResToDrawable(R.color.app_gray, view_config_ib_night_mode.drawable)
+        // Initialize switch
+        themeSwitch = view?.findViewById(R.id.theme_switch)!!
+
+        // Set initial state of the switch
+        themeSwitch.isChecked = isNightMode
+
+        // Set up the switch listener
+        themeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if(themeSwitch.isChecked) {
+                isNightMode = false
+                themeSwitch.isChecked = isChecked
+            } else {
+                isNightMode = true
+                themeSwitch.isChecked = isChecked
+            }
+            toggleBlackTheme()
+            setToolBarColor()
+            setAudioPlayerBackground()
+            AppUtil.saveConfig(activity, config)
+            EventBus.getDefault().post(ReloadDataEvent())
         }
+
+//        if (isNightMode) {
+//            view_config_ib_day_mode.isSelected = false
+//            view_config_ib_night_mode.isSelected = true
+//            UiUtil.setColorIntToDrawable(
+//                config.currentThemeColor,
+//                view_config_ib_night_mode.drawable
+//            )
+//            UiUtil.setColorResToDrawable(R.color.app_gray, view_config_ib_day_mode.drawable)
+//        } else {
+//            view_config_ib_day_mode.isSelected = true
+//            view_config_ib_night_mode.isSelected = false
+//            UiUtil.setColorIntToDrawable(
+//                config.currentThemeColor,
+//                view_config_ib_day_mode!!.drawable
+//            )
+//            UiUtil.setColorResToDrawable(R.color.app_gray, view_config_ib_night_mode.drawable)
+//        }
     }
 
     private fun inflateView() {
-
-        if (config.allowedDirection != Config.AllowedDirection.VERTICAL_AND_HORIZONTAL) {
-            view5.visibility = View.GONE
+//
+//        if (config.allowedDirection != Config.AllowedDirection.VERTICAL_AND_HORIZONTAL) {
+//            view5.visibility = View.GONE
 //            buttonVertical.visibility = View.GONE
 //            buttonHorizontal.visibility = View.GONE
-        }
+//        }
 
-        view_config_ib_day_mode.setOnClickListener {
-            isNightMode = true
-            toggleBlackTheme()
-            view_config_ib_day_mode.isSelected = true
-            view_config_ib_night_mode.isSelected = false
-            setToolBarColor()
-            setAudioPlayerBackground()
-            UiUtil.setColorResToDrawable(R.color.app_gray, view_config_ib_night_mode.drawable)
-            UiUtil.setColorIntToDrawable(config.currentThemeColor, view_config_ib_day_mode.drawable)
-            dialog?.hide()
-        }
-
-        view_config_ib_night_mode.setOnClickListener {
-            isNightMode = false
-            toggleBlackTheme()
-            view_config_ib_day_mode.isSelected = false
-            view_config_ib_night_mode.isSelected = true
-            UiUtil.setColorResToDrawable(R.color.app_gray, view_config_ib_day_mode.drawable)
-            UiUtil.setColorIntToDrawable(
-                config.currentThemeColor,
-                view_config_ib_night_mode.drawable
-            )
-            setToolBarColor()
-            setAudioPlayerBackground()
-            dialog?.hide()
-        }
+//        view_config_ib_day_mode.setOnClickListener {
+//            isNightMode = true
+//            toggleBlackTheme()
+//            view_config_ib_day_mode.isSelected = true
+//            view_config_ib_night_mode.isSelected = false
+//            setToolBarColor()
+//            setAudioPlayerBackground()
+//            UiUtil.setColorResToDrawable(R.color.app_gray, view_config_ib_night_mode.drawable)
+//            UiUtil.setColorIntToDrawable(config.currentThemeColor, view_config_ib_day_mode.drawable)
+//            dialog?.hide()
+//        }
+//
+//        view_config_ib_night_mode.setOnClickListener {
+//            isNightMode = false
+//            toggleBlackTheme()
+//            view_config_ib_day_mode.isSelected = false
+//            view_config_ib_night_mode.isSelected = true
+//            UiUtil.setColorResToDrawable(R.color.app_gray, view_config_ib_day_mode.drawable)
+//            UiUtil.setColorIntToDrawable(
+//                config.currentThemeColor,
+//                view_config_ib_night_mode.drawable
+//            )
+//            setToolBarColor()
+//            setAudioPlayerBackground()
+//            dialog?.hide()
+//        }
 
 //        if (activityCallback.direction == Config.Direction.HORIZONTAL) {
 //            buttonHorizontal.isSelected = true
 //        } else if (activityCallback.direction == Config.Direction.VERTICAL) {
 //            buttonVertical.isSelected = true
 //        }
-
+//
 //        buttonVertical.setOnClickListener {
 //            config = AppUtil.getSavedConfig(context)!!
 //            config.direction = Config.Direction.VERTICAL
@@ -178,11 +203,11 @@ class ConfigBottomSheetDialogFragment : BottomSheetDialogFragment() {
 
 //        buttonVertical.setTextColor(colorStateList)
 //        buttonHorizontal.setTextColor(colorStateList)
-
+//
         val adapter = FontAdapter(config, context!!)
-
+//
 //        view_config_font_spinner.adapter = adapter
-
+//
 //        view_config_font_spinner.background.setColorFilter(
 //            if (config.isNightMode) {
 //                R.color.night_default_font_color
